d3042649547686070e15e87d2a4fb3500fa41a13
David Konsumer <konsumer@jetboystudio.com>
2020-01-31 13:12:29 -0800

    intiial commit

---
+++ .arc
@@ -0,0 +1,7 @@
+@app
+jest-example
+
+@tables
+jesttest
+  PK *String        # Primary Identifier
+  SK **String       # ParamIndex1
---
+++ jest.test.js
@@ -0,0 +1,26 @@
+// this tests if arc tables & sandbox will work in jest
+
+/* global describe, it, expect, beforeAll, afterAll */
+
+// this is to get around no options for start, ala architect/architect/#621
+process.env.QUIET = true
+
+const sandbox = require('@architect/sandbox')
+
+// this uses hoisted-var to track the shutdown function for sandbox
+let end
+beforeAll(async () => {
+  console.log('Starting sandbox')
+  end = await sandbox.start()
+})
+afterAll(async () => {
+  console.log('Stopping sandbox')
+  await end()
+})
+
+describe('jest', () => {
+  it('should have started sandbox', () => {
+    expect(end).toBeDefined()
+    expect(typeof end).toEqual('function')
+  })
+})
