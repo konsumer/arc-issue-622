53b7f3433bf76b81cff10b20182b36d526a1313f
David Konsumer <konsumer@jetboystudio.com>
2020-02-12 21:10:03 -0800

    RC works for arc.tables()

--- jest.test.js
+++ jest.test.js
@@ -1,22 +1,19 @@
 // this tests if arc tables & sandbox will work in jest
 
 /* global describe, it, expect, beforeAll, afterAll */
-
+const arc = require('@architect/functions')
 const sandbox = require('@architect/sandbox')
-
-let tables
 
 beforeAll(async () => {
   await sandbox.start({ quiet: true })
-  const arc = require('@architect/functions')
-  tables = await arc.tables()
 })
 afterAll(async () => {
   await sandbox.end()
 })
 
 describe('jest', () => {
-  it('should know what tables are available', async () => {
-    expect(tables).toBeDefined()
+  it('be able to get table-list', async () => {
+    const data = await arc.tables()
+    expect(Object.keys(data)).toMatchSnapshot()
   })
 })
